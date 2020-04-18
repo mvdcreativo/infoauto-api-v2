df6313adb7ec4c9cf3237bc410294e07a8adc069
Emir <mvdcreativo@gmail.com>
2020-04-18 05:40:56 -0300

    prueba solucion problema numeros enteros response

--- app/Http/Controllers/Api/ProductController.php
+++ app/Http/Controllers/Api/ProductController.php
@@ -95,7 +95,7 @@ class ProductController extends Controller
             $product->save();
         }
 
-        return response()->json($product, 200);
+        return response()->json($product, 200,JSON_NUMERIC_CHECK);
     }
 
     /**
